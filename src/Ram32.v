57b21126e0176f48cefda70252731e609ffccbae
Victor Diniz <blek.net@gmail.com>
2015-09-17 16:14:36 -0300

    RC da Ram32.v, testbench

--- src/Ram32.v
+++ src/Ram32.v
@@ -1,6 +1,9 @@
+// Ram32.v - RAM de 128 palavras de 32 bits cada
+// (Adaptação do módulo Ram.v)
+
 module Ram (
-    input    [7:0]    addr      // Endereço a ser acessado
-    inout    [31:0]   data      // Dados lidos/a ser escritos
+    input    [7:0]    addr,     // Endereço a ser lido/escrito
+    inout    [31:0]   data,     // Dados lidos/a ser escritos
     input             wre       // 0: Leitura, 1: Escrita
 );
 
@@ -8,6 +11,14 @@ module Ram (
     wire    [31:0]    q;
 
     assign q = memory[addr];
+
+    // [!] Sobre interfaces bidirecionais (inout)
+    // Interfaces do tipo inout devem ser usadas interna e externamente como
+    // wires (nets), NUNCA como regs. Externamente, elas são legíveis quando o
+    // fio externo está definido como Z, e o interno como o valor de saída. O
+    // contrário ocorre na entrada: para escrever em uma interface bidirecional,
+    // é preciso definir o fio externo como o valor a ser escrito e definir o
+    // interno como Z.
     assign data[31:0]  = wre ? 32'hZZZZZZZZ : q[31:0];
 
     always @(wre or addr or data) begin
--- tb/ram32_tb0.v
+++ tb/ram32_tb0.v
@@ -1,29 +1,45 @@
+// Ram32_tb0.v - Testbench para o módulo Ram32
+
+`include "../src/Ram32.v"
+
 module Ram32_TB0;
-    reg [7:0] addr;
-    reg [31:0] data;
+    reg [7:0] addr; // Endereço a ser lido/escrito
+    wire [31:0] data; // Fio auxiliar - inout só interage com fios
+                      // (Definido como Z para leitura, definido como o valor
+                      // a ser escrito para escrita)
+    reg [31:0] data_r; // Valor a ser escrito
     reg wre;
-    reg chip_en = 1'b0;
 
     Ram ram (
         .addr(addr),
         .data(data),
-        .wre(wre),
-        .chip_en(chip_en)
+        .wre(wre)
     );
 
-initial begin
+    // Permite o uso da interface bidirecional (inout)
+    assign data[31:0] = wre ? data_r[31:0] : 32'hZZZZZZZZ;
+
+    initial begin
         $dumpfile("ram32_tb0.vcd");
         $dumpvars;
 
-        $display("\t\tData\tAddr\tWrite");
-        $monitor("\t%d", data); 
+        $display("Data\t\tAddr\tWrite");
+        $monitor("%d\t%d\t%d", data, addr, wre); 
 
         #500 $finish;
-    end_
+    end
 
     initial begin
         wre <= 1'b1;
-        addr <= 8'd0;
-        data <= 32'd455556;
+        addr[7:0] <= 8'h00;
+        data_r[31:0] <= 32'hBBBBBBBB;
+
+        #10 addr[7:0] <= 8'h01;
+        data_r[31:0] <= 32'hAAAAAAAA;
+
+        #10 wre <= 1'b0;
+        addr[7:0] <= 8'h00;
+
+        #500 $finish;
     end 
 endmodule
