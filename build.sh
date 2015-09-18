af4c6dd0dde79292511d3065a84cc3bd582cca8c
Victor Diniz <blek.net@gmail.com>
2015-09-18 20:17:25 -0300

    build.sh imprime arquivos compilados na stdout

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ BUILD_DIR="build/"
 # http://stackoverflow.com/a/15066129/3508191
 
 find $TB_DIR -type f -iname "*_tb*.v" -print0 | while IFS= read -r -d $'\0' line; do
-    out=$(basename "$line")
-    out=${out%.*}
-    $COMPILER -o ${BUILD_DIR}${out}.out $line
+    out=$(basename "$line");
+    out="${BUILD_DIR}${out%.*}.out";
+    echo "Compilando $line -> $out";
+    $COMPILER -o ${out} $line;
 done
