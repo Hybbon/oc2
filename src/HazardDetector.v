95fc92ce50ce89895345bc7a5c67b9997a694f75
Victor Diniz <blek.net@gmail.com>
2015-11-04 17:07:11 -0200

    Inicializa esqueleto do detector de hazard

---
+++ src/HazardDetector.v
@@ -0,0 +1,15 @@
+`ifndef HAZARDDETECTOR_V
+`define HAZARDDETECTOR_V
+
+module HazardDetector (
+    input [4:0] rega,
+    input [4:0] regb,
+    
+    output stalled 
+);
+
+
+
+endmodule
+
+`endif
