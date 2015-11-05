13f1e2e92a15cabd769eb5c466498bd8cbb5c808
Victor Diniz <blek.net@gmail.com>
2015-11-05 07:46:54 -0200

    Recupera Scoreboard com apenas uma interface síncrona

--- src/Scoreboard.v
+++ src/Scoreboard.v
@@ -6,38 +6,33 @@ module Scoreboard(
     input            reset,
 
     // inputs from issue stage, this is asynchronous
-    // 1st register
-    input      [4:0] ass_addr_a,      // requested register
-    output           ass_pending_a,   // register status
-    output     [1:0] ass_unit_a,      // register functional unit
-    output     [4:0] ass_row_a,       // register execution stage
-    // 2nd register
-    input      [4:0] ass_addr_b,      // requested register
-    output           ass_pending_b,   // register status
-    output     [1:0] ass_unit_b,      // register functional unit
-    output     [4:0] ass_row_b,       // register execution stage
+    input      [4:0] ass1_addr,      // requested register
+    output           ass1_pending,   // register status
+    output     [1:0] ass1_unit,      // register functional unit
+    output     [4:0] ass1_row,       // register execution stage
+
+    input      [4:0] ass2_addr,      // requested register
+    output           ass2_pending,   // register status
+    output     [1:0] ass2_unit,      // register functional unit
+    output     [4:0] ass2_row,       // register execution stage
 
     // inputs from issue stage
-    input      [1:0] registerunit,    // which functional unit it is going to
-    // 1st register
-    input      [4:0] writeaddr_a,     // register to become pending
-    input            enablewrite_a,    // prevent incorrect write (e.g. during stalls)
-    // 2nd register
-    input      [4:0] writeaddr_b,     // register to become pending
-    input            enablewrite_b    // prevent incorrect write (e.g. during stalls)
+    input      [4:0] writeaddr,     // register to become pending
+    input      [1:0] registerunit,  // which functional unit it is going to
+    input            enablewrite   // prevent incorrect write (e.g. during stalls)
 );
 
     reg [7:0] rows[31:0];
     reg [5:0] i;
 
     // outputs requested register data
-    assign ass_pending_a = rows[ass_addr_a][7];
-    assign ass_unit_a    = rows[ass_addr_a][6:5];
-    assign ass_row_a     = rows[ass_addr_a][4:0];
-
-    assign ass_pending_b = rows[ass_addr_b][7];
-    assign ass_unit_b    = rows[ass_addr_b][6:5];
-    assign ass_row_b     = rows[ass_addr_b][4:0];
+    assign ass1_pending = rows[ass1_addr][7];
+    assign ass1_unit    = rows[ass1_addr][6:5];
+    assign ass1_row     = rows[ass1_addr][4:0];
+    
+    assign ass2_pending = rows[ass2_addr][7];
+    assign ass2_unit    = rows[ass2_addr][6:5];
+    assign ass2_row     = rows[ass2_addr][4:0];
 
     always @(posedge clock or negedge reset) begin
         if(~reset) begin
@@ -58,20 +53,12 @@ module Scoreboard(
             end
 
             // writes registers claimed on issue stage
-            if(enablewrite_a) begin
-                // the corresponding registers becomes pending
+            if(enablewrite) begin
+                // the corresponding register becomes pending
                 // and we write its functional unity
-                rows[writeaddr_a][7] = 1'b1;
-                rows[writeaddr_a][6:5] = registerunit;
-                rows[writeaddr_a][4] = 1'b1;
-            end
-
-            if(enablewrite_b) begin
-                // the corresponding registers becomes pending
-                // and we write its functional unity
-                rows[writeaddr_b][7] = 1'b1;
-                rows[writeaddr_b][6:5] = registerunit;
-                rows[writeaddr_b][4] = 1'b1;
+                rows[writeaddr][7] = 1'b1;
+                rows[writeaddr][6:5] = registerunit;
+                rows[writeaddr][4] = 1'b1;
             end
 
         end
