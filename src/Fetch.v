94cdbb4505a5e7890baf811ca2cefad96319051a
Victor Diniz <blek.net@gmail.com>
2015-11-05 14:42:16 -0200

    Remove sinal inútil de stall do execute

--- src/Fetch.v
+++ src/Fetch.v
@@ -8,8 +8,6 @@ module Fetch (
     input                   reset,
     //Issue
     input                   iss_stall,
-    //Execute
-    input                   ex_if_stall,
     //Decode
     output reg    [31:0]    if_id_nextpc,
     output reg    [31:0]    if_id_instruc,
@@ -45,34 +43,29 @@ module Fetch (
             if_id_instruc <= 32'h0000_0000;
             pc <= 32'h0000_0000;
         end else begin
-            if (ex_if_stall) begin
-                if_id_instruc <= 32'h0000_0000;
+            if (iss_stall) begin
+                // issue stall should keep current instruction
                 if_id_nextpc <= pc;
             end else begin
-                if (iss_stall) begin
-                    // issue stall should keep current instruction
-                    if_id_nextpc <= pc;
+                if_id_instruc <= instr_data;
+                if (id_if_selpcsource) begin
+                    case (id_if_selpctype)
+                        2'b00: pc <= id_if_pcimd2ext;
+                        2'b01: pc <= id_if_rega;
+                        2'b10: pc <= id_if_pcindex;
+                        2'b11: pc <= 32'h0000_0040;
+                        default: pc <= 32'hXXXX_XXXX;
+                    endcase
+                    case (id_if_selpctype)
+                        2'b00: if_id_nextpc <= id_if_pcimd2ext;
+                        2'b01: if_id_nextpc <= id_if_rega;
+                        2'b10: if_id_nextpc <= id_if_pcindex;
+                        2'b11: if_id_nextpc <= 32'h0000_0040;
+						default: if_id_nextpc <= 32'hXXXX_XXXX;
+                        //default: pc <= 32'hXXXX_XXXX;
+                    endcase
                 end else begin
-                    if_id_instruc <= instr_data;
-                    if (id_if_selpcsource) begin
-                        case (id_if_selpctype)
-                            2'b00: pc <= id_if_pcimd2ext;
-                            2'b01: pc <= id_if_rega;
-                            2'b10: pc <= id_if_pcindex;
-                            2'b11: pc <= 32'h0000_0040;
-                            default: pc <= 32'hXXXX_XXXX;
-                        endcase
-                        case (id_if_selpctype)
-                            2'b00: if_id_nextpc <= id_if_pcimd2ext;
-                            2'b01: if_id_nextpc <= id_if_rega;
-                            2'b10: if_id_nextpc <= id_if_pcindex;
-                            2'b11: if_id_nextpc <= 32'h0000_0040;
-    						default: if_id_nextpc <= 32'hXXXX_XXXX;
-                            //default: pc <= 32'hXXXX_XXXX;
-                        endcase
-                    end else begin
-                        pc <= pc + 32'h0000_0004;
-                    end
+                    pc <= pc + 32'h0000_0004;
                 end
             end
         end
